d596e1c2a0408e4e3e2b05bf0eb723a2e43d68b4
dhimasSetyoWS <dhimas.palu@gmail.com>
2025-11-12 21:34:37 +0800

    Move checkoutpage to react

--- ecommerce-project/src/pages/HomePage.jsx
+++ ecommerce-project/src/pages/HomePage.jsx
@@ -1,9 +1,10 @@
-import "./Homepage.css"
-import "./header.css"
+import "./Homepage.css";
+import "./header.css";
 
 export function HomePage() {
   return (
     <>
+      <title>HomePage</title>
       <div className="header">
         <div className="left-section">
           <a href="index.html" className="header-link">
