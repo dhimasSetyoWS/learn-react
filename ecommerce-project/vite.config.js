398e1847baecb02d5ce853c456822aa2e3133536
dhimasSetyoWS <dhimas.palu@gmail.com>
2025-11-14 10:55:56 +0800

    add server proxy config for /api and /images

--- ecommerce-project/vite.config.js
+++ ecommerce-project/vite.config.js
@@ -4,4 +4,15 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server : {
+    proxy : {
+      // ini gunanya agar ketika ada request ke /api, maka dia akan redirect ke target yang ada di bawah
+      "/api" : {
+        target : "http://localhost:3000"
+      },
+      "/images" : {
+        target : "http://localhost:3000"
+      }
+    }
+  }
 })
